7eb97dcd1dedf54cbad9293af2c6fee973fc2ac0
alejandroEsc <jaescobar.cell@gmail.com>
2023-02-17 11:12:09 -0500

    feat: add new redpanda type to create redpanda clusters using a helm driver

--- src/go/k8s/main.go
+++ src/go/k8s/main.go
@@ -196,6 +196,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Console")
 		os.Exit(1)
 	}
+	if err = (&redpandacontrollers.RedpandaReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Redpanda")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("health", healthz.Ping); err != nil {
